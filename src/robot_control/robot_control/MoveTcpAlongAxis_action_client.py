cc7c6d919b04200b5422644897491695ad36a1f8
Mo <vboxuser@KassowRobot.myguest.virtualbox.org>
2024-12-04 09:23:20 +0100

    20241204_0923

--- src/robot_control/robot_control/MoveTcpAlongAxis_action_client.py
+++ src/robot_control/robot_control/MoveTcpAlongAxis_action_client.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
-from action_interfaces.action import MoveTcpAlongAxis  # Der Action-Typ
+from action_interfaces.action import MoveTcpAlongAxis
 
 class MoveTcpAlongAxisActionClient(Node):
 
@@ -16,24 +16,35 @@ class MoveTcpAlongAxisActionClient(Node):
         goal_msg.movement_axis = movement_axis
 
         self._action_client.wait_for_server()
-
-        goal_handle = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
-
-        return goal_handle
+        self._send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
 
     def feedback_callback(self, feedback_msg):
         feedback = feedback_msg.feedback
-        print(feedback)
         self.get_logger().info('Received feedback: {0}'.format(feedback.current_position))
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('ERROR - Movement request rejected!')
+            return
+
+        self.get_logger().info('Movement request accepted!')
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info('Movement successful!' if result.success else 'Movement ERROR!')
+        rclpy.shutdown()
+
 
 def main(args=None):
     rclpy.init(args=args)
-
     action_client = MoveTcpAlongAxisActionClient()
-    goal_handle = action_client.send_goal(0.0, "tcp", "axis_x")
-    result = goal_handle.result()  # Jetzt erhalten wir das Ergebnis
-
-    rclpy.shutdown()
+    action_client.send_goal(5.0, "tcp", "axis_x")
+    rclpy.spin(action_client)
 
 if __name__ == '__main__':
     main()
--- src/robot_control/robot_control/fibonacci_action_client.py
+++ src/robot_control/robot_control/fibonacci_action_client.py
@@ -1,32 +1,26 @@
 import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
-from action_interfaces.action import MoveTcpAlongAxis  # Dein Action-Typ
+from action_interfaces.action import Fibonacci
 
-class MoveTcpAlongAxisActionClient(Node):
 
+class FibonacciActionClient(Node):
     def __init__(self):
-        super().__init__('move_tcp_along_axis_action_client')
-        self._action_client = ActionClient(self, MoveTcpAlongAxis, 'move_tcp_along_axis')
+        super().__init__('fibonacci_action_client')
+        self._action_client = ActionClient(self, Fibonacci, 'fibonacci')
 
-    def send_goal(self, baseline, movement_frame, movement_axis):
-        goal_msg = MoveTcpAlongAxis.Goal()
-        goal_msg.baseline = baseline
-        goal_msg.movement_frame = movement_frame
-        goal_msg.movement_axis = movement_axis
+    def send_goal(self, order):
+        goal_msg = Fibonacci.Goal()
+        goal_msg.order = order
 
         self._action_client.wait_for_server()
 
-        self._send_goal_future = self._action_client.send_goal_async(
-            goal_msg,
-            feedback_callback=self.feedback_callback
-        )
+        self._send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
 
         self._send_goal_future.add_done_callback(self.goal_response_callback)
 
     def goal_response_callback(self, future):
         goal_handle = future.result()
-
         if not goal_handle.accepted:
             self.get_logger().info('Goal rejected :(')
             return
@@ -38,20 +32,23 @@ class MoveTcpAlongAxisActionClient(Node):
 
     def get_result_callback(self, future):
         result = future.result().result
-        self.get_logger().info('Result: Success = {0}'.format(result.success))
+        self.get_logger().info('Result: {0}'.format(result.sequence))
         rclpy.shutdown()
 
     def feedback_callback(self, feedback_msg):
         feedback = feedback_msg.feedback
-        self.get_logger().info('Received feedback: Current position = {0}'.format(feedback.current_position))
+        self.get_logger().info('Received feedback: {0}'.format(feedback.partial_sequence))
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    action_client = MoveTcpAlongAxisActionClient()
-    action_client.send_goal(0.0, "tcp", "x")
+    action_client = FibonacciActionClient()
+
+    action_client.send_goal(10)
+
     rclpy.spin(action_client)
+
 
 if __name__ == '__main__':
     main()
