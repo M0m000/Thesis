29125c3b16bf84ecfbb26ffb5151cabe54449c6a
M0m000 <moritz.arntz@live.com>
2025-03-05 11:51:56 +0100

    20250305_1151

--- src/robot_control/setup.py
+++ src/robot_control/setup.py
@@ -29,17 +29,15 @@ setup(
     license='TODO: License declaration',
     entry_points={
         'console_scripts': [
-            'move_lin_by_key = robot_control.move_lin_by_key:main',
             'MoveTcpAlongAxis_action_client = robot_control.MoveTcpAlongAxis_action_client:main',
             'tf_frames_publisher = robot_control.tf_frames_publisher:main',
             'ibvs_2d = robot_control.ibvs_2d:main',
             'set_working_frame_qr = robot_control.set_working_frame_qr:main',
             'define_working_frame_needle = robot_control.define_working_frame_needle:main',
-            'tcp_frame_listener = robot_control.tcp_frame_listener:main',
-            'transform_action_client = robot_control.transform_action_client:main',
             'scan_bar_horizontal_triangulation = robot_control.scan_bar_horizontal_triangulation:main',
             'scan_bar_vertical_triangulation = robot_control.scan_bar_vertical_triangulation:main',
             'scan_bar_combined_triangulation = robot_control.scan_bar_combined_triangulation:main',
+            'scan_bar = robot_control.scan_bar:main',
             'hook_data_listener = robot_control.hook_data_listener:main',
             'attachment_control_trajectory = robot_contro.attachment_control_trajectory:main',
             'attachment_control_servoing = robot_control.attachment_control_servoing:main'
