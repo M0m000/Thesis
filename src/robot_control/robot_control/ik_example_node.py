749c409655ba87917e6648271b2ef6ae656bcd91
M0m000 <moritz.arntz@live.com>
2025-04-10 15:26:16 +0200

    20250410_1526

--- src/robot_control/robot_control/ik_example_node.py
+++ src/robot_control/robot_control/ik_example_node.py
@@ -1,15 +1,12 @@
 import rclpy
 from rclpy.node import Node
-from FC.FC_ptp_movements import IKMoveExecutor  # Importiere deine Bewegungsklasse
+from FC.FC_call_move_ptp_service import MovePTPServiceClient
 
 class IKExampleNode(Node):
     def __init__(self):
         super().__init__('ik_example_node')
 
-        # Erstelle eine Instanz von IKMoveExecutor
-        self.ik_executor = IKMoveExecutor()
-
-        # Warte 2 Sekunden und führe dann die Bewegung aus
+        self.ik_executor = MovePTPServiceClient()
         self.timer = self.create_timer(2.0, self.send_example_request)
 
     def send_example_request(self):
@@ -23,7 +20,6 @@ class IKExampleNode(Node):
         except Exception as e:
             self.get_logger().error(f"Fehler bei Bewegung: {str(e)}")
 
-        # Timer beenden, damit nicht mehrfach aufgerufen wird
         self.timer.cancel()
 
     def shutdown(self):
@@ -45,3 +41,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
