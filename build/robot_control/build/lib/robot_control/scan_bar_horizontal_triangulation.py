af44e942cf0da895efa90f4178a4c8bfe303ae1e
M0m000 <moritz.arntz@live.com>
2025-04-11 13:45:35 +0200

    20250411_1345

--- build/robot_control/build/lib/robot_control/scan_bar_horizontal_triangulation.py
+++ build/robot_control/build/lib/robot_control/scan_bar_horizontal_triangulation.py
@@ -205,6 +205,7 @@ class ScanBarHorizontalTriangulation(Node):
         # self.get_logger().info("Wait 5 sec...")
         time.sleep(1)
         ###########################################################
+        
         self.scan_process_start_time = time.perf_counter()
         self.scan_process_end_time = None
     
