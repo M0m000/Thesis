f4ac77992c8e69d1e633bba4f8bf09f840aacc5e
M0m000 <moritz.arntz@live.com>
2025-05-08 17:08:26 +0200

    20250508_1708ä

--- install/robot_control/lib/python3.8/site-packages/robot_control/scan_bar_horizontal_triangulation.py
+++ install/robot_control/lib/python3.8/site-packages/robot_control/scan_bar_horizontal_triangulation.py
@@ -24,7 +24,7 @@ class ScanBarHorizontalTriangulation(Node):
     def __init__(self):
         super().__init__('scan_bar_horizontal_triangulation')
 
-        startpoint_trans_in_workframe = [0.0, -410.0, 30.0]      # Modell A und C
+        startpoint_trans_in_workframe = [-10.0, -410.0, 30.0]      # Modell A und C
         # startpoint_trans_in_workframe = [0.0, -390.0, 20.0]      # Modell B
         # startpoint_trans_in_workframe = [-30.0, -395.0, -40.0]      # Modell D
         startpoint_rot_in_workframe = [0.0, 0.0, 0.0]
